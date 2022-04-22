9d25b80441a169e87584817d2d01428f9baceef8
Mark <markwilkins86@gmail.com>
2022-04-22 16:43:44 -0400

    commit tree 7f59de7f62466e7371cc104ee8dea4de3a2b2677

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return 'This one is on me!'
+  } else if (feet <= 2000) {
+  return 'That will be twenty bucks.';
+  } else if (feet < 2500 && 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (feet != 2500) {
+    return 'No can do.';
+  }
+  feet(2501)
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  return city === 'NYC'?'Ok, sounds good.' : 'No go.'
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+}
+city('Pittsburgh')
+
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous' :
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    case 'thanks for everything' :
+      return 'Bye.'
+  }
 }
